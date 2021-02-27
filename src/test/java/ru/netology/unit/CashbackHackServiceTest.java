9c3b52da9352a3563b939c04e2836661cc2ce438
balrom1981 <balrom@bk.ru>
2021-02-27 15:30:35 +0300

    build&tests changed

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.unit;
 
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
@@ -27,6 +29,36 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void shouldCalculateMoreThan1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1100;
+        int expected = 900;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateSumLessThan1000Jupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateSumEqual1000Jupiter() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateMoreThan1000Jupiter() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1100;
         int expected = 900;
