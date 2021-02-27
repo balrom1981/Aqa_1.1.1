21d7f8d8740e237bfdc81006721eb7b7ff18bc72
balrom1981 <balrom@bk.ru>
2021-02-27 13:25:32 +0300

    build&tests changed

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,38 +1,39 @@
 package ru.netology.unit;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldCalculateSumLessThan1000() {
+    @org.testng.annotations.Test
+    public void shouldCalculateSumLessThan1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
         int expected = 100;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
 
-    @Test
-    void shouldCalculateSumEqual1000() {
+    @org.testng.annotations.Test
+    public void shouldCalculateSumEqual1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
 
-    @Test
-    void shouldCalculateMoreThan1000() {
+    @org.testng.annotations.Test
+    public void shouldCalculateMoreThan1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1100;
         int expected = 900;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
 
     }
 
