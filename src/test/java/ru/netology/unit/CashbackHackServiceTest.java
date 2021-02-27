320b286f55a4617ff1b4fa2764004045df6d6b5a
balrom1981 <balrom@bk.ru>
2021-02-27 13:39:12 +0300

    tests changed

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,39 +1,37 @@
 package ru.netology.unit;
 
 
-import org.testng.annotations.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.testng.Assert.assertEquals;
+public class CashbackHackServiceTest {
 
-class CashbackHackServiceTest {
-
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCalculateSumLessThan1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
         int expected = 100;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCalculateSumEqual1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCalculateMoreThan1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1100;
         int expected = 900;
         int actual = cashbackHackService.remain(amount);
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
 
     }
 
